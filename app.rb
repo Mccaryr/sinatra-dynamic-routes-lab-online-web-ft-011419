d4991a6917e3fdf4434400245fc8b6fbdfd2cc40
Robert McCary <robert.mccary@yahoo.com>
2019-02-26 16:24:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
     params[:number].to_i.times do
       answer += params[:phrase]
     end
-    answer 
+    answer
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
